f17cdef31f8bc959cc0eee4bd012a63ee686b201
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-07-08 16:57:03 +0900

    [change]enemymove

---
+++ Assets/Sakamoto/Scripts/EnemyData.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NewEnemyData",menuName ="Enemy/Enemy Data")]
+public class EnemyData : ScriptableObject
+{
+    public string TypeName;
+    public GameObject Prefab;
+    public float MoveSpeed;
+    public int Health;
+    public int AttackDamage;
+}
